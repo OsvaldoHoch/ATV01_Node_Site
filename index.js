2f74f56ab50a270ed8f88d7f7eebeca94c05dd8d
DSM2 <fatec-dsm2@academico.intra>
2026-09-21 19:41:41 -0300

    update

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use("/skates", SkateController);
 app.use("/pranchas", PranchaController);
 app.use("/patrocinados", PatrocinadosController);
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
