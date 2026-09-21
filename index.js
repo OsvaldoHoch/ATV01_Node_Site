4ac4b7b3b8511216c854afd1109df55d4c2a9169
DSM2 <fatec-dsm2@academico.intra>
2026-09-21 19:46:39 -0300

    update

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ app.use("/patrocinados", PatrocinadosController);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando em: http://localhost:${PORT}`);
 });
 
