9933d17ea1eaea89829749859a0b4742e702afa3
OsvaldoHoch <hildebrandhoch@gmail.com>
2026-09-21 16:00:17 -0300

    update

--- controllers/SkateController.js
+++ controllers/SkateController.js
@@ -3,12 +3,13 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
     const Skates = [
-        {medidas: "8.0", preco: "R$ 300", imagem: '/images/skateboard-1.png'},
-        {medidas: "7.9", preco: "R$ 250", imagem: '/images/skateboard-2.png'},
-        {medidas: "8.0", preco: "R$ 400", imagem: '/images/skateboard-3.png'},
-        {medidas: "8.0", preco: "R$ 500", imagem: '/images/skateboard-4.png'}
+        { medidas: "8.0", preco: "R$ 300", imagem: '/images/skateboard-1.png' },
+        { medidas: "7.9", preco: "R$ 250", imagem: '/images/skateboard-2.png' },
+        { medidas: "8.0", preco: "R$ 400", imagem: '/images/skateboard-3.png' },
+        { medidas: "8.0", preco: "R$ 500", imagem: '/images/skateboard-4.png' },
+        { medidas: "8.0", preco: "R$ 200", imagem: '/images/skateboard-5.png' }
     ];
-    res.render("Skates", {Skates: Skates});
+    res.render("Skates", { Skates: Skates });
 });
 
 export default router;
