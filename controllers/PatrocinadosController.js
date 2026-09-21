e30b9a709c9e7b24940117c1ecd5539bb95540ed
OsvaldoHoch <hildebrandhoch@gmail.com>
2026-09-20 23:44:25 -0300

    update

--- controllers/PatrocinadosController.js
+++ controllers/PatrocinadosController.js
@@ -1,16 +1,14 @@
 import express from "express";
 const router = express.Router();
 
-router.get("/Patrocinados", (req, res) => {
+router.get("/", (req, res) => {
     const Patrocinados = [
         {nome: "Luan Oliveira", cpf: "999.999.999-99"},
         {nome: "Sheni O'Neal", cpf: "999.999.999-99"},
         {nome: "Osvaldo", cpf: "999.999.999-99"},
         {nome: "Cofrinho", cpf: "999.999.999-99"}
     ];
-    res.render("Patrocinados", {
-        patrocinados: Patrocinados,
-    });
+    res.render("patrocinados");
 });
 
 export default router;
--- index.js
+++ index.js
@@ -7,19 +7,19 @@ import SkateController from "./controllers/SkateController.js"
 import PranchaController from "./controllers/PranchaController.js"
 import PatrocinadosController from "./controllers/PatrocinadosController.js"
 
+// Configurando EJS
+app.set("view engine", "ejs"); // Renderizar views
+app.use(express.static("public")); // Arquivos estáticos
+
 // Rota principal
 app.get("/", (req, res) => {
     res.render("index");
 });
 
-// Configurando EJS
-app.set("view engine", "ejs"); // Renderizar views
-app.use(express.static("public")); // Arquivos estáticos
-
 // Configurando rotas
-app.use("/", SkateController);
-app.use("/", PranchaController);
-app.use("/", PatrocinadosController);
+app.use("/skates", SkateController);
+app.use("/pranchas", PranchaController);
+app.use("/patrocinados", PatrocinadosController);
 
 const port = 8080;
 
